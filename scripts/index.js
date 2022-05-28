fa878f9f73f544aab5d2d127bf416ee47149d713
LevinaMaria <levinawork@yandex.ru>
2022-05-28 12:54:44 +0300

    feat: работа над пр5 процесс 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,25 @@
 const profile = document.querySelector(".profile");
 const popup = document.querySelector(".popup");
+
 // определить кнопку открытия попап
 const openPopupButton = document.querySelector(
   ".profile__change-button"
 );
 // определить кнопку закрытия попап
 const closePopupButton = popup.querySelector(".popup__close-button");
+
 // определить кнопку сохранения данных в форму
 const submitButton = popup.querySelector(".popup__submit-button");
+
 // найти форму и поля ввода
 const editProfile = document.querySelector(".popup__edit-profile");
 const nameInput = document.querySelector(".popup__item:first-child");
 const sublineInput = document.querySelector(".popup__item:nth-of-type(2)");
+
 // найти значения автор и О себе в профиле
 const profileAuthor = document.querySelector(".profile__author");
 const profileSubline = document.querySelector(".profile__subline");
+
 // шесть карточек из коробки
 const initialCards = [
   {
@@ -53,6 +58,7 @@ function togglePopup() {
        sublineInput.value = profileSubline.textContent;
    };
 };
+
 // назначить функцию отправки формы
 function formSubmitHandler (evt) {
       evt.preventDefault();
@@ -60,6 +66,7 @@ function formSubmitHandler (evt) {
     profileSubline.textContent = sublineInput.value;
     togglePopup();
 }
+
 // вывести функции
 openPopupButton.addEventListener("click", togglePopup);
 closePopupButton.addEventListener("click", togglePopup);
