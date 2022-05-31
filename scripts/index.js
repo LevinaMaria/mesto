d5f762f93fcd6c3209146acaefe3e5c2a3705534
LevinaMaria <levinawork@yandex.ru>
2022-05-31 14:58:50 +0300

    feat: работа над пр5 работа со скриптом. поправить функции

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,3 @@
-const profile = document.querySelector(".profile");
-const popup = document.querySelector(".popup");
-
-// определить кнопку открытия попап
-const openPopupButton = document.querySelector(
-  ".profile__change-button"
-);
-// определить кнопку закрытия попап
-const closePopupButton = popup.querySelector(".popup__close-button");
-
-// определить кнопку сохранения данных в форму
-const submitButton = popup.querySelector(".popup__submit-button");
-
-// найти форму и поля ввода
-const editProfile = document.querySelector(".popup__edit-profile");
-const nameInput = document.querySelector(".popup__item:first-child");
-const sublineInput = document.querySelector(".popup__item:nth-of-type(2)");
-
-// найти значения автор и О себе в профиле
-const profileAuthor = document.querySelector(".profile__author");
-const profileSubline = document.querySelector(".profile__subline");
-
 // шесть карточек из коробки
 const initialCards = [
   {
@@ -48,27 +26,105 @@ const initialCards = [
   }
 ];
 
-// назначить переключатель открытия и закрытия попап и заполнения попап при открытии данными из профиля
-function togglePopup() {
-   if (popup.classList.contains("popup_opened")){
-       popup.classList.toggle('popup_opened');
-   } else {
-       popup.classList.toggle('popup_opened');
-       nameInput.value = profileAuthor.textContent;
-       sublineInput.value = profileSubline.textContent;
-   };
+const page = document.querySelector(".page"); //страница
+const profile = page.querySelector(".profile"); // профиль (секция с именем и данными пользователя)
+const popup = page.querySelector(".popup"); // все модальные окна
+const popupEditAuthor = page.querySelector('.popup-edit-author'); // попап редактирования профиля (1)
+const popupEditCard = page.querySelector('.popup-edit-card'); // попап добавления карточки (2)
+const popupViewImage = page.querySelector('.popup-view-image'); // попап просмотра картинки (3)
+const openAuthorPopupBtn = profile.querySelector(".profile__change-button"); // открытие окна профиля (1)
+const openCardPopupBtn = profile.querySelector(".profile__add-button"); // открытие окна добавления карточки (2)
+const closePopupButtons = page.querySelectorAll(".popup__close-button"); // кнопка закрытия попап (любого)
+const submitButton = page.querySelectorAll(".popup__submit-button"); // кнопка сохранения данных в форму (в любом окне)
+const editProfile = popupEditAuthor.querySelector(".popup__edit-profile"); // форма редактирования профиля (1)
+const nameInput = popupEditAuthor.querySelector(".popup__item:first-child"); // поле редактирования имени (1)
+const sublineInput = popupEditAuthor.querySelector(".popup__item:nth-of-type(2)"); // поле редактирования подписи (1)
+const profileAuthor = profile.querySelector(".profile__author"); // первоначальное значение имени в профиле (1)
+const profileSubline = profile.querySelector(".profile__subline"); // первоначальное значение подписи в профиле (1)
+const popupImgUrl = popupEditCard.querySelector('.popup__item_image-url'); // ссылка картинки в карточке (2)
+const popupImgName = popupEditCard.querySelector('.popup__item_image-name'); // имя картинки в карточке (2)
+const cardsElements = page.querySelector('.elements'); // все карточки с картинками (2)
+const templateElement = page.querySelector('.element-template').content; // привязать темплейт к карточке (2)
+
+function renderCard(name, link) { //функция передачи данных карточке
+  const cardElement = templateElement.cloneNode(true); // клонируем темплейт
+  let cardCaption = cardElement.querySelector('.element__caption');
+  let cardImage = cardElement.querySelector('.element__image');
+  cardCaption.textContent = name;
+  cardImage.src = link;
+  cardImage.alt = name;
+
+  let likeImage = cardElement.querySelector(".element__like-button"); // кнопка лайк в карточке
+  likeImage.addEventListener('click', () => {
+    likeImage.classList.toggle('element__like-button_active'); // назначить слушатель события нажатия кнопки лайк и присвоения по клику класса "Актив"
+  });
+
+  let deleteCard = cardElement.querySelector(".element__trash-button"); // кнопка удаления карточки
+  deleteCard.addEventListener('click', () => {
+    let deletedEelement = deletedEelement.closest(".element"); // назначить выбор удаляемой карточки
+    deletedEelement.remove(); // удалить карточку
+  });
+
+  cardImage.addEventListener('click', () => { // слушатель события клика на картинку
+    popupViewImage.classList.toggle("popup_opened"); // открытие попапа по клику на картинку (3)
+    let imageView = popupViewImage.querySelector(".popup__view-image"); // картинка в третьем попапе (3)
+    let imageViewTitle = popupViewImage.querySelector(".popup__image-title"); // подпись картинки в попакпе (3)
+    imageView.src = link;
+    imageView.alt = name;
+    imageViewTitle.textContent = name;
+  })
+
+  cardsElements.append(cardElement);
 };
 
-// назначить функцию отправки формы
-function formSubmitHandler (evt) {
-      evt.preventDefault();
-    profileAuthor.textContent = nameInput.value;
-    profileSubline.textContent = sublineInput.value;
-    togglePopup();
-}
+// передать данные карточек из констант
+initialCards.reverse().forEach(item => {
+  let name = item.name;
+  let link = item.link;
 
-// вывести функции
-openPopupButton.addEventListener("click", togglePopup);
-closePopupButton.addEventListener("click", togglePopup);
+  renderCard(name, link);
+});
+
+closePopupButtons.forEach( item => {
+  item.addEventListener ('click', (evt) => {
+    const eventTarget = evt.target;
+    const closePopupButton = eventTarget.closest('.popup');
+
+    toggleForm(closePopupButton);
+  });
+});
+
+function toggleForm(element) {
+  element.classList.toggle("popup_opened");
+};
+
+function openedProfile () {
+  nameInput.value = profileAuthor.textContent;
+  sublineInput.value = profileSubline.textContent;
+
+  toggleForm(popupEditAuthor);
+};
+
+// назначить функцию отправки формы автора
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileAuthor.textContent = nameInput.value;
+  profileSubline.textContent = sublineInput.value;
+  
+  toggleForm(popupEditAuthor);
+};
+
+// добавить функцию заполнения карточки пользователем
+function cardSubmitHandler (evt) {
+  evt.preventDefault();
+  let newPopupImgName = popupImgName.value;
+  let newPopupImgUrl = popupImgUrl.value;
+  renderCard(newPopupImgName, newPopupImgUrl);
+
+  toggleForm(popupEditCard);
+};
+
+
+
 
 editProfile.addEventListener('submit', formSubmitHandler);
